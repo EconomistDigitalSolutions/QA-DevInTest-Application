dbd07329fca36d7c9357cfd92b085168809a0535
Ammar Al-Taweel <gameforkstudio@gmail.com>
2019-03-14 17:37:58 +0000

    Add a test to check that the search fields
    The test checks that the search fields exist on the page.

--- features/home.feature
+++ features/home.feature
@@ -3,3 +3,7 @@ Feature: Jobs page
   Scenario: Navigation bar renders
     Given I go to the jobs page
     Then I should see the navigation bar
+
+  Scenario: Search fields render
+    Given I go to the jobs page
+    Then I should see the search fields
--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -7,3 +7,7 @@ Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
 })
+
+Then(/^I should see the search fields$/, async () => {
+  return World.driver.findElement(By.className('search'));
+})
