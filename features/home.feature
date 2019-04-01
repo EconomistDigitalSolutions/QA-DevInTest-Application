c642a37977bf098387df950666bed8ff80e41d02
Pawan Kumar <pawan_kumar95@hotmail.com>
2019-04-01 19:57:34 +0100

    All checks for rendering complete

--- features/home.feature
+++ features/home.feature
@@ -3,3 +3,27 @@ Feature: Jobs page
   Scenario: Navigation bar renders
     Given I go to the jobs page
     Then I should see the navigation bar
+
+  Scenario: Search fields render
+    Given I go to the jobs page
+    Then I should see search fields
+
+  Scenario: Sector lists
+    Given I go to the jobs page
+    Then I should see sector lists
+
+  Scenario: Jobs blog
+    Given I go to the jobs page
+    Then I should see jobs blog
+
+  Scenario: Featured jobs
+    Given I go to the jobs page
+    Then I should see featured jobs
+
+  Scenario: Footer should render
+    Given I go to the jobs page
+    Then I should see the footer
+
+  Scenario: Sign in link
+    Given I click on sign in
+    Then I should see login page
--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -6,4 +6,40 @@ Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
-})
+});
+
+Then('I should see search fields', async () => {
+           // Write code here that turns the phrase above into concrete actions
+           return World.driver.findElement(By.id('keywords'));
+         });
+
+Then('I should see sector lists', async () => {
+    // Write code here that turns the phrase above into concrete actions
+            return World.driver.findElement(By.className('browse__items'));
+        });
+
+Then('I should see jobs blog', async () => {
+    // Write code here that turns the phrase above into concrete actions
+             return World.driver.findElement(By.className('articles'));
+      });
+
+Then('I should see featured jobs', async () => {
+      // Write code here that turns the phrase above into concrete actions
+      return World.driver.findElement(By.className('featured-jobs__item'));
+    });
+
+
+Then('I should see the footer', async () => {
+      // Write code here that turns the phrase above into concrete actions
+        return World.driver.findElement(By.css('footer'));
+      });
+
+Given('I click on sign in', async () => {
+              // Write code here that turns the phrase above into concrete actions
+              return World.driver.findElement(By.id(''));
+            });
+
+Then('I should see login page', async () => {
+              // Write code here that turns the phrase above into concrete actions
+                  return 'pending';
+               });
