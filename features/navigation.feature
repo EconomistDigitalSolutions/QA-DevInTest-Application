4e07a8f9850b91724346182c4d172eb2aa2c7050
Pawan Kumar <pawan_kumar95@hotmail.com>
2019-04-03 11:09:58 +0100

    navigation links checked

---
+++ features/navigation.feature
@@ -0,0 +1,26 @@
+Feature: Test navigation links
+
+Scenario: Home link
+  Given I go to the jobs page
+  When I click home
+  Then Redirects to home page
+
+Scenario: Find a job link
+  Given I go to the jobs page
+  When I click Find a job
+  Then Redirects to Find a job page
+
+  Scenario: Job alerts link
+    Given I go to the jobs page
+    When I click Job alerts
+    Then Redirects to Job alerts page
+
+    Scenario: Search recruiters link
+      Given I go to the jobs page
+      When I click Search recruiters
+      Then Redirects to Search recruiters page
+
+      Scenario: Jobs blog link
+        Given I go to the jobs page
+        When I click Jobs blog
+        Then Redirects to Jobs blog
--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -1,6 +1,14 @@
-const { Given, When, Then } = require('cucumber');
-const { By } = require('selenium-webdriver');
-const World = require('../support/world');
+const { Given, When, Then } = require("cucumber");
+const { By } = require("selenium-webdriver");
+const World = require("../support/world");
+const assert = require("assert");
+
+
+$HOME = "https://jobs.economist.com/";
+$FIND_JOB = "https://jobs.economist.com/jobs/";
+$JOB_ALERT = "https://jobs.economist.com/newalert/";
+$SEARCH_RECTRUITER = "https://jobs.economist.com/employers/";
+$JOBS_BLOG = "https://jobs.economist.com/careers/";
 
 Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 
@@ -53,3 +61,85 @@ Then('I should see create account page', function () {
            // Write code here that turns the phrase above into concrete actions
            return World.driver.findElement(By.id('create-account'));
         });
+
+When('I click home', function () {
+
+   // Write code here that turns the phrase above into concrete actions
+   return World.driver.findElement(By.linkText('Home')).click();
+ });
+
+
+ Then('Redirects to home page', async () => {
+   // assert.equal(World.driver.getCurrentUrl(), $HOME);
+   // String CurrentUrl = World.driver.getCurrentUrl();
+   // World.driver.wait(1000*3);
+
+     await World.driver.getCurrentUrl().then(function(url){
+       assert.equal(url, $HOME);
+     });
+   // World.driver.getCurrentUrl().then(url => {console.log('current url: "' + url + '"');});
+   // var currentUrl = World.driver.getCurrentUrl().toString();
+
+   // assert.equal(currentUrl, "https://jobs.economist.com");
+   // var gettingCurrent = World.driver.getCurrentUrl();
+   // assert.equal(gettingCurrent, $HOME);
+//
+//    var currentUrl;
+//
+//   World.driver.getCurrentUrl().then(function () {
+//   assert.equal(currentUrl, $HOME);
+// });
+
+// String URL = World.driver.getCurrentUrl();
+// Assert.assertEquals(URL, "https://jobs.economist.com/" );
+
+ });
+
+
+ When('I click Find a job', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return World.driver.findElement(By.linkText('Find a job')).click();
+ });
+
+ Then('Redirects to Find a job page', async () => {
+   // Write code here that turns the phrase above into concrete actions
+   await World.driver.getCurrentUrl().then(function(url){
+     assert.equal(url, $FIND_JOB);
+   });
+  });
+
+ When('I click Job alerts', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return World.driver.findElement(By.linkText('Job alerts')).click();
+ });
+
+ Then('Redirects to Job alerts page', async () => {
+   // Write code here that turns the phrase above into concrete actions
+   await World.driver.getCurrentUrl().then(function(url){
+     assert.equal(url, $JOB_ALERT);
+   });
+  });
+
+ When('I click Search recruiters', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return World.driver.findElement(By.linkText('Search recruiters')).click();
+ });
+
+ Then('Redirects to Search recruiters page', async () => {
+   // Write code here that turns the phrase above into concrete actions
+   if (World.driver.getCurrentUrl() ==  $SEARCH_RECTRUITER) {
+     return World.driver.getCurrentUrl();
+   }
+ });
+
+ When('I click Jobs blog', function () {
+   // Write code here that turns the phrase above into concrete actions
+   return World.driver.findElement(By.linkText('Jobs blog')).click();
+ });
+
+  Then('Redirects to Jobs blog', async () => {
+   // Write code here that turns the phrase above into concrete actions
+   await World.driver.getCurrentUrl().then(function(url){
+     assert.equal(url, $JOBS_BLOG);
+   });
+  });
