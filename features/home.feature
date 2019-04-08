aebe612bcd6722b9099fbaed56090fa8957f7555
Marko Honkanen <marko@markoh.co.uk>
2019-04-08 10:08:16 +0100

    Add steps for search fields

--- features/home.feature
+++ features/home.feature
@@ -3,3 +3,14 @@ Feature: Jobs page
   Scenario: Navigation bar renders
     Given I go to the jobs page
     Then I should see the navigation bar
+
+  # Note: This could be done as a scenario outline but checking all
+  # fields in one scenario is quicker as the page is not reloaded
+  # between checks and it can be argued it's one test. 
+  # Choosing one or the other depends on preference.
+  Scenario: Search fields rendered
+    Given I go to the jobs page
+    Then I should see the search form
+    And I should see the search field for "keyword"
+    And I should see the search field for "location"
+    And I should see the search field for "distance"
--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -1,8 +1,32 @@
 const { Given, Then } = require('cucumber');
 const { By } = require('selenium-webdriver');
+const { expect } = require('chai');
 const World = require('../support/world');
 
 Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 
 Then(/^I should see the navigation bar$/,
   async () => World.driver.findElement(By.id('primary-nav')));
+
+const searchFields = {
+  keyword: 'field--keyword',
+  location: 'field--location',
+  distance: 'field--radial-select',
+};
+
+Then(/^I should see the search form$/, async () => {
+  const displayed = await World.driver
+    .findElement(By.className('search__form'))
+    .isDisplayed();
+
+  expect(displayed).to.eql(true, 'Search field not displayed');
+});
+
+Then(/^I should see the search field for "(.*)"$/, async (fieldName) => {
+  const fieldVisible = await World.driver
+    .findElement(By.className('search__form'))
+    .findElement(By.className(searchFields[fieldName]))
+    .isDisplayed();
+
+  expect(fieldVisible).to.eql(true, `Search field for ${fieldName} not displayed`);
+});
