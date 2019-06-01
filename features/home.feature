578a633dc8f36c18f3522beb3315d991d6a5d15b
JRIngram <jr-ingram@hotmail.co.uk>
2019-06-01 11:50:14 +0100

    Updated tests. Added notes to document some choices

--- features/home.feature
+++ features/home.feature
@@ -3,23 +3,25 @@ Feature: Jobs page
   Scenario: Navigation bar renders
     Given I go to the jobs page
     Then I should see the navigation bar
-    
+
   Scenario: Search fields renderers
   	Given I go to the jobs page
-  	Then I should see the search fields
-  
+  	Then I should see the keywords field
+    And I should see the location field
+    And I should see the radial location field
+
   Scenario: Sector lists renders
   	Given I go to the jobs page
   	Then I should see the sector list
-  
+
   Scenario: Jobs blog renders
   	Given I go to the jobs page
   	Then I should see the jobs blog
-  
+
   Scenario: Featured jobs render
-	Given I go to the jobs page
-  	Then I should see the features jobs
-  	
+	  Given I go to the jobs page
+  	Then I should see the featured jobs
+
   Scenario: Footer renders
-	Given I go to the jobs page
-	Then I should see the footer
+	  Given I go to the jobs page
+	  Then I should see the footer
--- features/step_definitions/index.js
+++ features/step_definitions/index.js
@@ -7,3 +7,38 @@ Given(/^I go to the jobs page$/, () => World.goToJobsPage());
 Then(/^I should see the navigation bar$/, async () => {
   return World.driver.findElement(By.id('primary-nav'));
 })
+
+Then (/^I should see the keywords field$/, async () =>{
+  return World.driver.findElement(By.id('keywords'));
+})
+
+Then (/^I should see the location field$/, async () =>{
+  return World.driver.findElement(By.id('location'));
+})
+
+Then (/^I should see the radial location field$/, async () =>{
+  return World.driver.findElement(By.id('RadialLocation'));
+})
+
+Then (/^I should see the sector list/, async () =>{
+  /*
+  * Couldn't find any clear way to identify Browse jobs by Sector.
+  * This appeared to be the best option?
+  */
+  return World.driver.findElement(By.className('browse__items'));
+})
+
+Then (/^I should see the jobs blog/, async () =>{
+  /**
+  * Similar to sector list, couldn't find a clear way to identify.
+  */
+  return World.driver.findElement(By.className('articles brick'));
+})
+
+Then (/^I should see the featured jobs/, async () =>{
+  return World.driver.findElement(By.className('featured-jobs brick'));
+})
+
+Then (/^I should see the footer$/, async () =>{
+  return World.driver.findElement(By.tagName('footer'));
+})
